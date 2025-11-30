6ca47586ce435426c3d53781013e25a6cb05fbd0
Souvik <souvikelric@gmail.com>
2025-11-30 21:04:51 +0530

    fixing tidy function

--- command/tidy.go
+++ command/tidy.go
@@ -5,8 +5,16 @@ import (
 	"path/filepath"
 )
 
+// function to check if provided folder name exists
+// if it doesnt it creates the folder
+func checkPath(folderName string){
+	_,err := os.Stat(folderName)
+	if os.IsNotExist(err){
+		os.MkdirAll(folderName,os.ModePerm)
+	}
+}
+
 func TidyFiles(dirPath string) error {
-	// Implementation for tidying files goes here
 	files,err := os.ReadDir(dirPath)
 
 	if err != nil {
@@ -14,32 +22,27 @@ func TidyFiles(dirPath string) error {
 	}
 
 	for _,file := range files {
-		// get all image files and move them to Images folder
-		// get all document files and move them to Documents folder
-		// get all video files and move them to Videos folder
-		// get all audio files and move them to Audio folder
-		// get all other files and move them to Others folder
 
 		if !file.IsDir() {
 			// create folders if not exist
 			// move files accordingly
-			os.MkdirAll(dirPath+"/Images", os.ModePerm)
-			os.MkdirAll(dirPath+"/Documents", os.ModePerm)
-			os.MkdirAll(dirPath+"/Videos", os.ModePerm)
-			os.MkdirAll(dirPath+"/Audio", os.ModePerm)
-			os.MkdirAll(dirPath+"/Others", os.ModePerm)
 
 			ext := filepath.Ext(file.Name())
 			switch ext {
 			case ".jpg", ".jpeg", ".png", ".gif", ".bmp":
+				checkPath(dirPath+"/Images")
 				os.Rename(filepath.Join(dirPath, file.Name()), filepath.Join(dirPath, "Images", file.Name()))
 			case ".pdf", ".doc", ".docx", ".txt", ".xls", ".xlsx", ".ppt", ".pptx":
+				checkPath(dirPath+"/Documents")
 				os.Rename(filepath.Join(dirPath, file.Name()), filepath.Join(dirPath, "Documents", file.Name()))
 			case ".mp4", ".mkv", ".avi", ".mov":
+				checkPath(dirPath+"/Videos")
 				os.Rename(filepath.Join(dirPath, file.Name()), filepath.Join(dirPath, "Videos", file.Name()))
 			case ".mp3", ".wav", ".aac":
+				checkPath(dirPath+"/Audio")
 				os.Rename(filepath.Join(dirPath, file.Name()), filepath.Join(dirPath, "Audio", file.Name()))
 			default:
+				checkPath(dirPath+"/Others")
 				os.Rename(filepath.Join(dirPath, file.Name()), filepath.Join(dirPath, "Others", file.Name()))
 			}
 		}
